4b430b6880c4aa81bb1ca9c682799d70a7d04c2b
Marcus Vinicius <marcusviniciuso@outlook.com.br>
2020-10-17 20:44:09 -0300

    Novos Pokes

--- Pokemon/src/Rpg/caterpie.java
+++ Pokemon/src/Rpg/caterpie.java
@@ -1,31 +1,92 @@
 package Rpg;
 
 public class Caterpie extends PokemonAr {
-		private int nivel;
-		private String dono;
+	private int nivel = 0;
+	private String dono;
+	private String[] nomes = {"Caterpie","Metapod","Butterfree"};
+	
+	public Caterpie(String dono) {
+		super( 
+				"Caterpie",
+				113, // vida
+				55, // ataque
+				55,  // defesa
+				0, // velocidade
+				2.9, // peso
+				0.3, // altura
+				0, // xpMIn - escluir
+				100,
+				"Caterpie"
+		);
+		this.dono=dono;
+	}
+
+	public int getNivel() {
+		return nivel;
+	}
+
+	public void setNivel(int nivel) {
+		this.nivel = nivel;
+	}
+
+	public String getDono() {
+		return dono;
+	}
+
+	public void setDono(String dono) {
+		this.dono = dono;
+	}
+	
+	public void recebeXp(int xp){ //atualizar nivel
+		this.setXpMin( this.getXpMin() + xp );
 		
-		public Caterpie(String nome, double vida, double ataque, double defesa, double velocidade, double peso,
-				double altura, double xpMin, double xpMax, String tipo, int nivel, String dono) 
-		{
-			super(nome, vida, ataque, defesa, velocidade, peso, altura, xpMin, xpMax, tipo);
-			this.nivel = nivel;
-			this.dono = dono;
+		if( this.getXpMin() >= this.getXpMax() && (this.nomes.length-1)>this.nivel) {
+			this.setXpMin(0);
+			this.nivel++;
+			involuir(nivel);
 		}
-
-		public int getNivel() {
-			return nivel;
+	}
+	
+	public void involuir(int nivel) {
+		this.setNome(nomes[nivel]);
+		switch(nivel) {
+		case 1:
+			this.setVida(120);
+			this.setAtaque(45);
+			this.setDefesa(80);
+			this.setAltura(0.7);
+			this.setPeso(9.9);
+		break;
+		case 2:
+			this.setVida(134);
+			this.setAtaque(167);
+			this.setDefesa(137);
+			this.setAltura(1.1);
+			this.setPeso(32);
 		}
-
-		public void setNivel(int nivel) {
-			this.nivel = nivel;
+	}
+	
+	public double ataque(){
+		return this.rand(this.getAtaque(), this.getAtaque() );
+	}
+	
+	public double defesa(){
+		return this.rand(this.getDefesa(), this.getDefesa() );
+	}
+	
+	public void recebeDano(double dano) {
+		double newDano = this.defesa() - dano; 
+		if( newDano>0 ) {
+			this.setVida( this.getVida()  - ( newDano ) );
 		}
-
-		public String getDono() {
-			return dono;
+		else {
+			System.out.println("O pokémon "+this.getNome()+" não recebeu nenhum dano!");
 		}
-
-		public void setDono(String dono) {
-			this.dono = dono;
-		}
-
+	}
+	
+	
+	private double rand(double i, double j) {
+		return  (i) - ( Math.random() * (j) );
+	}
+	
 }
