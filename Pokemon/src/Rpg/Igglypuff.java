f8d7d177fb955d8efbeea84c141f4980b72a6f9e
Vanessa <vanmtv@hotmail.com>
2020-10-17 15:00:01 -0300

    "Testar Pokemon"

--- Pokemon/src/Rpg/Igglypuff.java
+++ Pokemon/src/Rpg/Igglypuff.java
@@ -4,8 +4,8 @@ public class Igglypuff extends PokemonFada{
 	private int nivel;
 	private String dono;
 	
-	public Igglypuff(String nome, double vida, double ataque, double defesa, double velocidade, double peso, double altura, double xpMin, double xpMax) {
-		super(nome, vida, ataque, defesa, velocidade, peso, altura, xpMin, xpMax);
+	public Igglypuff(String nome, double vida, double ataque, double defesa, double velocidade, double peso, double altura, double xpMin, double xpMax, int nivel, String dono, String tipo) {
+		super(nome, vida, ataque, defesa, velocidade, peso, altura, xpMin, xpMax, tipo);
 		this.nivel = nivel;
 		this.dono = dono;
 	}
